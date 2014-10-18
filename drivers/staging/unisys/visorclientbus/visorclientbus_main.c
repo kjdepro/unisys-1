cfe7a5293cf2891c25ff33cf7340473b99a9bfa4
Erik Arfvidson <erik.arfvidson@unisys.com>
2014-10-18 17:02:20 -0400

    staging: unisys: fixable all strict checks and warning for visorclientbus_main.c
    I modified a variable goto Away; to goto away:
    
    Signed-off-by: Erik Arfvidson <erik.arfvidson@unisys.com>

--- drivers/staging/unisys/visorclientbus/visorclientbus_main.c
+++ drivers/staging/unisys/visorclientbus/visorclientbus_main.c
@@ -45,7 +45,7 @@ static void chipset_device_resume(ulong busNo, ulong devNo);
 /** These functions are implemented herein, and are called by the chipset
  *  driver to notify us about specific events.
  */
-static VISORCHIPSET_BUSDEV_NOTIFIERS Chipset_Notifiers = {
+static VISORCHIPSET_BUSDEV_NOTIFIERS chipset_notifiers = {
 	.bus_create = chipset_bus_create,
 	.bus_destroy = chipset_bus_destroy,
 	.device_create = chipset_device_create,
@@ -58,10 +58,10 @@ static VISORCHIPSET_BUSDEV_NOTIFIERS Chipset_Notifiers = {
 /** These functions are implemented in the chipset driver, and we call them
  *  herein when we want to acknowledge a specific event.
  */
-static VISORCHIPSET_BUSDEV_RESPONDERS Chipset_Responders;
+static VISORCHIPSET_BUSDEV_RESPONDERS chipset_responders;
 
 /* filled in with info about parent chipset driver when we register with it */
-static struct ultra_vbus_deviceinfo Chipset_DriverInfo;
+static struct ultra_vbus_deviceinfo chipset_driver_info;
 
 static void __iomem *
 get_virt(u64 phys_addr, u32 bytes, VISORCHIPSET_ADDRESSTYPE addrType)
@@ -70,7 +70,7 @@ get_virt(u64 phys_addr, u32 bytes, VISORCHIPSET_ADDRESSTYPE addrType)
 		if (phys_addr > virt_to_phys(high_memory - 1)) {
 			ERRDRV("%s - bad localTest address for channel (0x%-16.16Lx for %lu bytes)",
 			       __func__,
-			       (unsigned long long) phys_addr, (ulong) bytes);
+			       (unsigned long long)phys_addr, (ulong)bytes);
 			return NULL;
 		}
 		return (void __iomem *)__va(phys_addr);
@@ -91,9 +91,8 @@ otherwise return NULL.
 		p = &root->child;
 		for (;;) {
 			tmp = *p;
-			if (!tmp
-			    || tmp->start > (phys_addr + bytes - 1)) {
-				/*Memory region is undefined */
+			if (!tmp || tmp->start > (phys_addr + bytes - 1)) {
+				/* Memory region is undefined */
 				break;
 			}
 			p = &tmp->sibling;
@@ -104,8 +103,8 @@ otherwise return NULL.
 				/*Memory is reserved and within HIGH_MEMORY */
 				ERRDRV("%s - localPhysical address overlaps memory our OS is currently using! (0x%-16.16Lx for %lu bytes)",
 				       __func__,
-				       (unsigned long long) phys_addr,
-				       (ulong) bytes);
+				       (unsigned long long)phys_addr,
+				       (ulong)bytes);
 				return NULL;
 			}
 			break;	/* greater then HIGH_MEMORY */
@@ -113,16 +112,16 @@ otherwise return NULL.
 		/* come out, if Memory is undefined or greater then
 		 * HIGM_MEMORY
 		 */
-		if (phys_addr > (u64) ULONG_MAX) {
+		if (phys_addr > (u64)ULONG_MAX) {
 			ERRDRV("%s - localPhysical address is too large to be be mapped (0x%-16.16Lx for %lu bytes)",
 			       __func__,
-			       (unsigned long long) phys_addr, (ulong) bytes);
+			       (unsigned long long)phys_addr, (ulong)bytes);
 			return NULL;
 		}
-		pcpy = ioremap_cache((ulong) phys_addr, (ulong) bytes);
+		pcpy = ioremap_cache((ulong)phys_addr, (ulong)bytes);
 		if (pcpy == NULL) {
 			ERRDRV("%s - ioremap_cache(0x%lx,%lu) failed",
-			       __func__, (ulong) phys_addr, (ulong) bytes);
+			       __func__, (ulong)phys_addr, (ulong)bytes);
 			return NULL;
 		}
 		return pcpy;
@@ -131,58 +130,58 @@ otherwise return NULL.
 }
 
 static void __iomem *
-chipset_preamble(ulong busNo, ulong devNo, VISORCHIPSET_DEVICE_INFO *devInfo)
+chipset_preamble(ulong busNo, ulong devNo, VISORCHIPSET_DEVICE_INFO *devinfo)
 {
-	if (!visorchipset_get_device_info(busNo, devNo, devInfo)) {
+	if (!visorchipset_get_device_info(busNo, devNo, devinfo)) {
 		ERRDRV("%s - visorchipset_get_device_info returned false",
 		       __func__);
 		return NULL;
 	}
-	if ((uuid_le_cmp(devInfo->chanInfo.channelTypeGuid,
+	if ((uuid_le_cmp(devinfo->chanInfo.channelTypeGuid,
 			 spar_vnic_channel_protocol_uuid) != 0) &&
-	    (uuid_le_cmp(devInfo->chanInfo.channelTypeGuid,
+	    (uuid_le_cmp(devinfo->chanInfo.channelTypeGuid,
 			 spar_vhba_channel_protocol_uuid) != 0)) {
 		ERRDRV("%s - I only know how to handle VNIC or VHBA client channels",
-		     __func__);
+		       __func__);
 		return NULL;
 	}
-	return get_virt(devInfo->chanInfo.channelAddr,
-			devInfo->chanInfo.nChannelBytes,
-			devInfo->chanInfo.addrType);
+	return get_virt(devinfo->chanInfo.channelAddr,
+			devinfo->chanInfo.nChannelBytes,
+			devinfo->chanInfo.addrType);
 }
 
 static void
 chipset_bus_create(ulong busNo)
 {
 	int rc = 0;
-	u64 channelAddr = 0;
-	ulong nChannelBytes = 0;
-	VISORCHIPSET_BUS_INFO busInfo;
+	u64 channeladdr = 0;
+	ulong nchannelbytes = 0;
+	VISORCHIPSET_BUS_INFO businfo;
 	CONTROLVM_MESSAGE msg;
 
 	POSTCODE_LINUX_3(BUS_CREATE_ENTRY_PC, busNo, POSTCODE_SEVERITY_INFO);
-	if ((visorchipset_get_bus_info(busNo, &busInfo)) &&
-	    (busInfo.chanInfo.channelAddr > 0) &&
-	    (busInfo.chanInfo.nChannelBytes > 0)) {
-		channelAddr = busInfo.chanInfo.channelAddr;
-		nChannelBytes = (ulong) busInfo.chanInfo.nChannelBytes;
+	if ((visorchipset_get_bus_info(busNo, &businfo)) &&
+	    (businfo.chanInfo.channelAddr > 0) &&
+	    (businfo.chanInfo.nChannelBytes > 0)) {
+		channeladdr = businfo.chanInfo.channelAddr;
+		nchannelbytes = (ulong)businfo.chanInfo.nChannelBytes;
 	}
 	/* Save off message with IOVM bus info in case of crash */
-	if ((uuid_le_cmp(busInfo.chanInfo.channelInstGuid,
+	if ((uuid_le_cmp(businfo.chanInfo.channelInstGuid,
 			 spar_siovm_uuid) == 0)) {
 		msg.hdr.Id = CONTROLVM_BUS_CREATE;
 		msg.hdr.Flags.responseExpected = 0;
 		msg.hdr.Flags.server = 0;
 		msg.cmd.createBus.busNo = busNo;
-		msg.cmd.createBus.deviceCount = busInfo.devNo;
-		msg.cmd.createBus.channelAddr = channelAddr;
-		msg.cmd.createBus.channelBytes = nChannelBytes;
+		msg.cmd.createBus.deviceCount = businfo.devNo;
+		msg.cmd.createBus.channelAddr = channeladdr;
+		msg.cmd.createBus.channelBytes = nchannelbytes;
 		dump_vhba_bus = busNo;
 		visorchipset_save_message(&msg, CRASH_bus);
 	}
 
 	if (!uislib_client_inject_add_bus(busNo, UltraVbusChannelProtocolGuid,
-					  channelAddr, nChannelBytes)) {
+					  channeladdr, nchannelbytes)) {
 		rc = -1;
 	}
 
@@ -195,8 +194,8 @@ chipset_bus_create(ulong busNo)
 		POSTCODE_LINUX_3(BUS_CREATE_FAILURE_PC, busNo,
 				 POSTCODE_SEVERITY_ERR);
 	}
-	if (Chipset_Responders.bus_create)
-		(*Chipset_Responders.bus_create) (busNo, rc);
+	if (chipset_responders.bus_create)
+		(*chipset_responders.bus_create) (busNo, rc);
 }
 
 static void
@@ -211,25 +210,25 @@ chipset_bus_destroy(ulong busNo)
 		INFODRV("%s(%lu) successful", __func__, busNo);
 	else
 		ERRDRV("%s(%lu) failed", __func__, busNo);
-	if (Chipset_Responders.bus_destroy)
-		(*Chipset_Responders.bus_destroy) (busNo, rc);
+	if (chipset_responders.bus_destroy)
+		(*chipset_responders.bus_destroy) (busNo, rc);
 }
 
 static void
 chipset_device_create(ulong busNo, ulong devNo)
 {
-	void __iomem *pAddr = NULL;
+	void __iomem *paddr = NULL;
 	int rc = 0;
 	VISORCHIPSET_DEVICE_INFO devInfo;
 	CONTROLVM_MESSAGE msg;
 
-	pAddr = chipset_preamble(busNo, devNo, &devInfo);
+	paddr = chipset_preamble(busNo, devNo, &devInfo);
 	POSTCODE_LINUX_4(DEVICE_CREATE_ENTRY_PC, devNo, busNo,
 			 POSTCODE_SEVERITY_INFO);
 
-	if (!pAddr) {
+	if (!paddr) {
 		rc = -1;
-		goto Away;
+		goto away;
 	}
 	if (!uuid_le_cmp(devInfo.chanInfo.channelTypeGuid,
 			 spar_vnic_channel_protocol_uuid)) {
@@ -240,9 +239,9 @@ chipset_device_create(ulong busNo, ulong devNo)
 		     devInfo.chanInfo.addrType == ADDRTYPE_localTest,
 		     devInfo.devInstGuid, &devInfo.chanInfo.intr)) {
 			rc = -2;
-			goto Away;
+			goto away;
 		}
-		goto Away;
+		goto away;
 	} else if (!uuid_le_cmp(devInfo.chanInfo.channelTypeGuid,
 				spar_vhba_channel_protocol_uuid)) {
 		/* Save off message with hba info in case of crash */
@@ -270,13 +269,13 @@ chipset_device_create(ulong busNo, ulong devNo)
 		     devInfo.chanInfo.addrType == ADDRTYPE_localTest,
 		     devInfo.devInstGuid, &devInfo.chanInfo.intr)) {
 			rc = -3;
-			goto Away;
+			goto away;
 		}
-		goto Away;
+		goto away;
 	}
 
 	rc = -4;		/* unsupported GUID */
-Away:
+away:
 	if (rc >= 0) {
 		INFODRV("%s(%lu,%lu) successful", __func__, busNo, devNo);
 		POSTCODE_LINUX_4(DEVICE_CREATE_SUCCESS_PC, devNo, busNo,
@@ -286,103 +285,103 @@ Away:
 		POSTCODE_LINUX_4(DEVICE_CREATE_FAILURE_PC, devNo, busNo,
 				 POSTCODE_SEVERITY_ERR);
 	}
-	if (Chipset_Responders.device_create)
-		(*Chipset_Responders.device_create) (busNo, devNo, rc);
+	if (chipset_responders.device_create)
+		(*chipset_responders.device_create) (busNo, devNo, rc);
 }
 
 static void
 chipset_device_destroy(ulong busNo, ulong devNo)
 {
-	void __iomem *pAddr = NULL;
+	void __iomem *paddr = NULL;
 	int rc = 0;
 	VISORCHIPSET_DEVICE_INFO devInfo;
 
-	pAddr = chipset_preamble(busNo, devNo, &devInfo);
-	if (!pAddr) {
+	paddr = chipset_preamble(busNo, devNo, &devInfo);
+	if (!paddr) {
 		rc = -1;
-		goto Away;
+		goto away;
 	}
 	if (!uuid_le_cmp(devInfo.chanInfo.channelTypeGuid,
 			 spar_vnic_channel_protocol_uuid)) {
 		uislib_client_inject_del_vnic(busNo, devNo);
-		goto Away;
+		goto away;
 	} else if (!uuid_le_cmp(devInfo.chanInfo.channelTypeGuid,
 				spar_vhba_channel_protocol_uuid)) {
 		uislib_client_inject_del_vhba(busNo, devNo);
-		goto Away;
+		goto away;
 	}
 	rc = -1;		/* no match on GUID */
-Away:
+away:
 	if (rc >= 0)
 		INFODRV("%s(%lu,%lu) successful", __func__, busNo, devNo);
 	else
 		ERRDRV("%s(%lu,%lu) failed", __func__, busNo, devNo);
-	if (Chipset_Responders.device_destroy)
-		(*Chipset_Responders.device_destroy) (busNo, devNo, rc);
+	if (chipset_responders.device_destroy)
+		(*chipset_responders.device_destroy) (busNo, devNo, rc);
 }
 
 static void
 chipset_device_pause(ulong busNo, ulong devNo)
 {
-	void __iomem *pAddr = NULL;
+	void __iomem *paddr = NULL;
 	int rc = 0;
 	VISORCHIPSET_DEVICE_INFO devInfo;
 
-	pAddr = chipset_preamble(busNo, devNo, &devInfo);
-	if (!pAddr) {
+	paddr = chipset_preamble(busNo, devNo, &devInfo);
+	if (!paddr) {
 		rc = -1;
-		goto Away;
+		goto away;
 	}
 	if (!uuid_le_cmp(devInfo.chanInfo.channelTypeGuid,
 			 spar_vnic_channel_protocol_uuid)) {
 		rc = uislib_client_inject_pause_vnic(busNo, devNo);
-		goto Away;
+		goto away;
 	} else if (!uuid_le_cmp(devInfo.chanInfo.channelTypeGuid,
 				spar_vhba_channel_protocol_uuid)) {
 		rc = uislib_client_inject_pause_vhba(busNo, devNo);
-		goto Away;
+		goto away;
 	}
 	rc = -1;		/* no match on GUID */
-Away:
+away:
 	if (rc == CONTROLVM_RESP_SUCCESS)
 		INFODRV("%s(%lu,%lu) successful", __func__, busNo, devNo);
 	/* Response sent when the pause is completed */
 	else {
 		ERRDRV("%s(%lu,%lu) failed", __func__, busNo, devNo);
-		if (Chipset_Responders.device_pause)
-			(*Chipset_Responders.device_pause) (busNo, devNo, rc);
+		if (chipset_responders.device_pause)
+			(*chipset_responders.device_pause) (busNo, devNo, rc);
 	}
 }
 
 static void
 chipset_device_resume(ulong busNo, ulong devNo)
 {
-	void __iomem *pAddr = NULL;
+	void __iomem *paddr = NULL;
 	int rc = 0;
 	VISORCHIPSET_DEVICE_INFO devInfo;
 
-	pAddr = chipset_preamble(busNo, devNo, &devInfo);
-	if (!pAddr) {
+	paddr = chipset_preamble(busNo, devNo, &devInfo);
+	if (!paddr) {
 		rc = -1;
-		goto Away;
+		goto away;
 	}
 	if (!uuid_le_cmp(devInfo.chanInfo.channelTypeGuid,
 			 spar_vnic_channel_protocol_uuid)) {
 		rc = uislib_client_inject_resume_vnic(busNo, devNo);
-		goto Away;
+		goto away;
 	} else if (!uuid_le_cmp(devInfo.chanInfo.channelTypeGuid,
 				spar_vhba_channel_protocol_uuid)) {
 		rc = uislib_client_inject_resume_vhba(busNo, devNo);
-		goto Away;
+		goto away;
 	}
 	rc = -1;		/* no match on GUID */
-Away:
+away:
 	if (rc == CONTROLVM_RESP_SUCCESS)
 		INFODRV("%s(%lu,%lu) successful", __func__, busNo, devNo);
 	else
 		ERRDRV("%s(%lu,%lu) failed", __func__, busNo, devNo);
-	if (Chipset_Responders.device_resume)
-		(*Chipset_Responders.device_resume) (busNo, devNo, rc);
+	if (chipset_responders.device_resume)
+		(*chipset_responders.device_resume) (busNo, devNo, rc);
 }
 
 static int __init
@@ -393,9 +392,9 @@ visorclientbus_init(void)
 	/* This enables us to receive notifications when devices appear for
 	 * which this service partition is to be a client for.
 	 */
-	visorchipset_register_busdev_client(&Chipset_Notifiers,
-					    &Chipset_Responders,
-					    &Chipset_DriverInfo);
+	visorchipset_register_busdev_client(&chipset_notifiers,
+					    &chipset_responders,
+					    &chipset_driver_info);
 
 	POSTCODE_LINUX_2(CHIPSET_INIT_EXIT_PC, POSTCODE_SEVERITY_INFO);
 
